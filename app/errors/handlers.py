45ce9fdf8f6185ced33fd1f33ae47aa8d0a528cb
Bill.Wu <jiazhuangle@gmail.com>
2019-07-19 10:30:36 +0800

    update error handler

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,14 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from app import db
 from app.errors import bp
+from app.api.errors import error_response as er
 
+
+def needs_json():
+    return request.accecpt_mimetypes['application/json'] >= \
+           request.accept_mimetypes['text/html']
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    if needs_json():
+        return er(404)
     return render_template('errors/404.html'), 404
-
 
 @bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
+    if needs_json():
+        return er(500)
     return render_template('errors/500.html'), 500
